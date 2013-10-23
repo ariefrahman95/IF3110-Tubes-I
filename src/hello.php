1602f4e81f5f87da000d905ef7c1c7f23dd9b6b5
Azalea Fisitania <hanato.kobato121@gmail.com>
2013-10-23 19:30:17 +0700

    23/10 Sangat Layout Kasar (Pure HTML5 aja)
    Layoutnya pun belum lengkap, malem ini mau dilengkapin lagi :3
    Baca aja lengkapnya di docs/Cek Spek Tubes WBD :D kalo ada perubahan
    catet juga ya di situ :D

---
+++ src/hello.php
@@ -0,0 +1,49 @@
+<!--
+Tugas Besar Web Based Development
+Kelompok TripleA:
+Akbar Syam 13511048
+Azalea Fisitania 13511028
+Arief Rahman 13511020
+-->
+
+<!--author Azal-->
+<!DOCTYPE HTML> <!--typical HTML5-->
+<html>
+	
+	<!--HEAD-->
+	<!--Make A Title To Display on Browser Tab-->
+	<head>
+		<title>Ruko Serba Ada</title>
+	</head>
+	
+	<!--BODY--> 
+	<body>
+
+		<!--Logo Situs-->
+  		<p>
+  			<a href="hello.php">
+  				<img src = "ruserba.png"/>
+  			</a>
+  		</p>
+  		<?php echo '<p> Selamat Datang di Ruko Serba Ada</p>' ?> 
+  		
+  		<!--Kategori-->
+  		<p>
+  			<a href="kategori/makanan.php">Makanan</a> &nbsp;
+  			<a href="kategori/minuman.php">Minuman</a>  &nbsp;
+  			<a href="kategori/perlengkapanmandi.php">Perlengkapan Mandi</a> &nbsp;
+  			<a href="kategori/perlengkapanbayi.php">Perlengkapan Bayi</a> &nbsp;
+  			<a href="kategori/alattuliskantor.php">Alat Tulis Kantor</a> &nbsp;
+  		</p>
+
+
+
+  		<!--Header & Konten-->
+  		<h2>Judul Artikel</h2>
+  		<p>
+  			Konten artikel, blah blah blah
+  		</p>
+
+	</body>
+
+</html>
